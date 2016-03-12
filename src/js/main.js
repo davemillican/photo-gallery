6f0706581a23f3e6eb4d48adb672ef9f832c4343
David Millican <dave_millican@yahoo.com>
2016-03-12 09:14:48 -0500

    fixed multiple instances

--- src/js/main.js
+++ src/js/main.js
@@ -1,194 +1,114 @@
-// (function () {
 
-//     var ul = $('ul');
+(function () {
 
-//     ul.on('click', function(e) {
-//         var target = $(e.target);
+    function PhotoGallery (element, pix) {
 
-//         if (target.is('li')) {
-//             target.remove();
-//         } else if (target.parents('li').lenght > 0) {
-//             target.parents('li').remove();
-//         }
-//     });
+        // define some necessary variables
 
-//     $('button').on('click', function() {
-//         var newLi = $('<li>');
-//         newLi.html('yo ');
-//         ul.append(newLi);
-//     });
-  
-// })();
+        this.gallery = $('<div>',{class:'gallery'});
+
+        $(element).append(this.gallery);
+        // Add the Big picture element.
+        this.currentWindow = $('<img>', {class:'gallery-current'});
+        this.gallery.append (this.currentWindow);
+
+        // Add the Scroll Bar below 
+        this.scrollWindow = $('<div>',{class:'gallery-scroll'});
+        this.gallery.append (this.scrollWindow);
+
+        // Utility functions:tools working with images image.
+        this.setCurrentSrc = function (url) {
+            this.currentWindow.attr('src',url);
+        };
+
+        //  This will take an image (url) and return a JQuery
+        //  object with the url in an img element.
+        function addImage (url) {
+            var image =$('<img>');
+            image.attr('src',url);
+            return image;        
+        }
+
+        //  ***********   Presentation Section ***********
+        //  The next section of code will setup the Gallery 
+        //  for presentation. 
+
+        //  Produce a ScrollElements container that will 
+        //  contain all the images .
+        var scrollElements = pix.reduce (function(prev,next) {
+            var image =$('<img>');
+            image.attr('src',next);
+            return prev.add(image);
+        },$());
+
+
+        this.scrollWindow.append(scrollElements);
+        this.setCurrentSrc(pix[0]);
+
+
+        //  ********   API Section  ********************
+        // API: the following functions will be returned to the
+        // caller to allow the user to control the workings of 
+        //the gallery
+
+        var destroyFunction = function destroyFunction( ) {
+            this.scrollWindow.remove();
+            this.currentWindow.remove();
+
+        }
+        var addImageFunction = function addImageFuncton(newImg) {
+      
+            this.scrollWindow.append (addImage(newImg));  
+        }
+
+        var removeImageFunction = function removeImageFunction(imgStr) {
+            var toRemove = scrollWindow.find('[src="'+imgStr+'"]');
+
+            toRemove.remove();     
+        }
+
+        // ***********   Wrap Up Section 
+        // **************************************
+
+
+        //  Create a Event Listener that will pickup any 
+        //  mouse clicks on the Scroll List.
+        var saved = this;
+        this.scrollWindow.on('click', function (e) {
+            if($(e.target).is('img')) {
+                saved.currentWindow.attr('src',$(e.target).attr('src'));
+            }
+        });
+
+        this.destroy = destroyFunction;
+        this.addImage = addImageFunction;
+        this.removeImage = removeImageFunction;    
+
+    }
 
     var images = ['src/bldg.jpg', 'src/skyscraper-day-fun.jpg','src/bldg.jpg', 'src/skyscraper-day-fun.jpg','src/bldg.jpg', 'src/skyscraper-day-fun.jpg']
 
-function photoGallery (element, pix) {
+    var shoot = new PhotoGallery ($('.container'), images);
 
-    // define some necessary variables.
-    var refObject = {};
-
-    var gallery = $(element);
-
-    // Add the Big picture element.
-    var current = $('<img>', {class:'gallery-current'});
-    gallery.append (current);
-
-    // Add the Scroll Bar below 
-    var scroll = $('<div>',{class:'gallery-scroll'});
-    gallery.append (scroll);
-
-    // Utility functions:tools working with images image.
-    function setCurrentSrc (url) {
-        current.attr('src',url);
-    }
-
-    //  This will take an image (url) and return a JQuery
-    //  object with the url in an img element.
-    function addImage (url) {
-        var image =$('<img>');
-        image.attr('src',next);
-        return image;        
-    }
-
-    //  ***********   Presentation Section ***********
-    //  The next section of code will setup the Gallery 
-    //  for presentation. 
-
-    //  Produce a ScrollElements container that will 
-    //  contain all the images .
-    var scrollElements = images.reduce (function(prev,next) {
-        var image =$('<img>');
-        image.attr('src',next);
-        return prev.add(image);
-    },$());
-
-
-    scroll.append(scrollElements);
-    setCurrentSrc(images[0]);
-
-    //  ********   API Section  ********************
-    // API: the following functions will be returned to the
-    // caller to allow the user to control the workings of 
-    //the gallery
-
-    var destroyFunction = function destroyFunction( ) {
-        scroll.remove();
-        current.remove();
-
-    }
-    var addImageFunction = function addImageFuncton(newImg) {
-        scroll.append (addImage);
-    
-    }
-
-    var removeImageFunction = function removeImageFunction(imgStr) {
-   
-    }
-
-    // ***********   Wrap Up Section 
-    // **************************************
-    refObject.destroy = destroyFunction;
-    refObject.addImage = addImageFunction;
-    refObject.removeImage = removeImageFunction;
-
-    return refObject;    
-
-}
-
-    var images = ['src/bldg.jpg', 'src/skyscraper-day-fun.jpg','src/bldg.jpg', 'src/skyscraper-day-fun.jpg','src/bldg.jpg', 'src/skyscraper-day-fun.jpg']
-
-    var shoot = photoGallery ($('.gallery'), images);
-
+    shoot.addImage('src/tiger2.jpg');
+    shoot.addImage('src/tiger1.jpg');
+    //shoot.removeImage('src/tiger1.jpg');
     //shoot.destroy();
 
+    var shoot2 = PhotoGallery ($('.container2'), images);
+    //$('body').append(shoot2);
+
+    for (var i=0;i<10;i++){
+        console.log(i);
+    }
+
+
+})();
 
 //*****************************************************
 //*****************************************************
 //*****************************************************
 //*****************************************************
-function Holdoff () { 
-
-    var images = ['src/bldg.jpg', 'src/skyscraper-day-fun.jpg','src/bldg.jpg', 'src/skyscraper-day-fun.jpg','src/bldg.jpg', 'src/skyscraper-day-fun.jpg']
-
-    var container = $('#gallery');
-
-    var current = container.find('.gallery-current');
-    var scroll = container.find('.gallery-scroll');
-
-    function setCurrentSrc (url) {
-        current.attr('src',url);
-    }
-
-    //  ********   API Section  ********************
-    // API: the following functions will be returned to the
-    // caller to allow the user to control the workings of 
-    //the gallery
-
-    var destroyFunction = function destroyFunction( ) {
-    }
-    var addImageFunction = function addImageFuncton(newImg) {
-        // body...
-    }
-
-    var removeImageFunction = function removeImageFunction(img) {
-    }
-
-
-
-    //  ***********   Presentation Section ***********
-    //  The next section of code will setup the Gallery 
-    //  for presentation. 
-
-    //  Produce a ScrollElements container that will 
-    //  contain all the images .
-    var scrollElements = images.reduce (function(prev,next) {
-        var image =$('<img>');
-        image.attr('src',next);
-        return prev.add(image);
-    },$());
-
-
- 
-
-    scroll.append(scrollElements);
-    setCurrentSrc(images[0]);
-
-    //  Create a Event Listener that will pickup any 
-    //  mouse clicks on the Scroll List.
-    $('.gallery-scroll').on('click', function (e) {
-        if($(e.target).is('img')) {
-            current.attr('src',$(e.target).attr('src'));
-        }
-    });
-
-
-    // ***********   Wrap Up Section 
-    // **************************************
-    refObject.destroy = destroyFunction;
-    refObject.addImage = addImageFunction;
-    refObject.removeImage = removeImageFunction;
-
-    return refObject;
-}
-/*  Notes from 11:20am 
-var gpage = photoGallery ($('#gallery'),arr);
-
-PhotoGallery.prototype.destroy ()
-PhotoGallery.prototype.addImage ()
-PhotoGallery.prototype.removeImage ()
-
-
-function photoGallery () {
-    return {
-        destroy:
-        addImage:
-        removeImage:
-    }
-}
-
-$("[src ='+string']")
-
-*/
 
 
 
